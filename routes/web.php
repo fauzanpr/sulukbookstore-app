65106d1493025060b5625cd78534ca100a773e6a
fauzanpr <fauzanpr.06@gmail.com>
2022-12-14 14:27:36 +0700

    add force true on /fresh

--- routes/web.php
+++ routes/web.php
@@ -73,8 +73,8 @@ Route::post('/admin/login', [LoginController::class, 'admin_authenticate'])->mid
 // LOGOUT ROUTE
 Route::post('/logout', [LogoutController::class, 'logout'])->middleware('auth');
 
-Route::get('fresh', function (){
-    Artisan::call('migrate:fresh --seed');
+Route::get('/fresh', function () {
+    Artisan::call('migrate:fresh', [
+       '--force' => true
+    ]);
 });
-
-
