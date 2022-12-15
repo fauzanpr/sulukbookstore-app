b49b42cea1fcfb1eb8a59337b91589fd38e55d53
fauzanpr <fauzanpr.06@gmail.com>
2022-12-15 11:30:09 +0700

    update on meta

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -2,10 +2,10 @@
 <html lang="en">
 
 <head>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Landing Page</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <!--===============================================================================================-->
     <link rel="icon" type="image/png" href="{{ asset('images/icons/favicon.png') }}" />
     <!--===============================================================================================-->
